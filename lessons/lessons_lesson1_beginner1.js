820cccb27bad9daaf7a35225d1e2f6691cae1ac3
pllato <59840270+pllato@users.noreply.github.com>
2025-06-03 12:05:12 +0500

    Update lessons_lesson1_beginner1.js

--- lessons/lessons_lesson1_beginner1.js
+++ lessons/lessons_lesson1_beginner1.js
@@ -3,11 +3,11 @@ addLesson({
   lesson: "lesson1",
   name: "Урок 1",
   structures: [
-    { structure: "I am [name].", pattern: ["i", "am", "name"], id: "s1", hasName: true },
-    { structure: "You are [name].", pattern: ["you", "are", "name"], id: "s2", hasName: true },
-    { structure: "What is your name?", pattern: ["what", "is", "your", "name"], id: "s3", hasName: false },
-    { structure: "My name is [name].", pattern: ["my", "name", "is", "name"], id: "s4", hasName: true },
-    { structure: "This is [name].", pattern: ["this", "is", "name"], id: "s5", hasName: true }
+    { structure: "I am [name].", pattern: ["i", "am", "name"], translation: "Я [имя].", id: "s1", hasName: true },
+    { structure: "You are [name].", pattern: ["you", "are", "name"], translation: "Ты [имя].", id: "s2", hasName: true },
+    { structure: "What is your name?", pattern: ["what", "is", "your", "name"], translation: "Как тебя зовут?", id: "s3", hasName: false },
+    { structure: "My name is [name].", pattern: ["my", "name", "is", "name"], translation: "Моё имя [имя].", id: "s4", hasName: true },
+    { structure: "This is [name].", pattern: ["this", "is", "name"], translation: "Это [имя].", id: "s5", hasName: true }
   ],
   requiredCorrect: 2, // 2 correct examples per structure
   validateStructure: function(text, structure) {
